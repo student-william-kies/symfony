db0b316ec963d5f3fa4323977d6d2f122d427492
William² <william.kies@laplateforme.io>
2021-09-24 17:16:06 +0200

    manque plus qu'a créer des posts

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -2,18 +2,45 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\UpdateUserType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class ProfilController extends AbstractController
 {
     #[Route('/profil', name: 'profil')]
-    public function index(Request $request): Response
+    public function index(Request $request, UserPasswordEncoderInterface $passwordEncoder): Response
     {
+        $user = $this -> getUser();
+        $form = $this -> createForm(UpdateUserType::class, $user);
+
+        $form -> handleRequest($request);
+        if ($form -> isSubmitted() && $form -> isValid())
+        {
+            $user->setPassword(
+                $passwordEncoder->encodePassword(
+                    $user,
+                    $form->get('password')->getData()
+                )
+            );
+
+            $manage = $this -> getDoctrine() -> getManager();
+
+            $manage -> persist($user);
+            $manage -> flush();
+
+            $this -> addFlash('message', 'Votre profil à bien été mise à jour');
+            return $this -> redirect('/logout');
+        }
+
+
         return $this->render('profil/profil.html.twig', [
             'controller_name' => 'ProfilController',
+            'updateForm' => $form -> createView()
         ]);
     }
 }
---
+++ src/Form/UpdateUserType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class UpdateUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('login')
+            ->add('password', PasswordType::class, [
+                // instead of being set onto the object directly,
+                // this is read and encoded in the controller
+                'mapped' => false,
+                'attr' => ['autocomplete' => 'new-password'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'S\'il vous plait, entrez un mot de passe',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre mot de passe doit contenir {{ limit }} caractères',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
